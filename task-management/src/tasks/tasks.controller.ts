31a6c905d2d58fd45221d113a7e0c47cf695568f
ApsaraWitharana <asachini095@gmail.com>
2025-04-29 19:18:55 +0530

    add-create task api coll

--- task-management/src/tasks/tasks.controller.ts
+++ task-management/src/tasks/tasks.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post,Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+import { title } from 'process';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private taskService:TasksService){
 
     }
+
+    //TODO:GetAll Task
     @Get()
-    getAllTasks(){
+    getAllTasks():Task[] {
       return this.taskService.getAllTask()
+    }
+
+    //TODO:Create Task
+    @Post()
+    createTasks(
+        @Body('title') title:string,
+        @Body('description') description:string
+    ):Task{
+    return this.taskService.createTask(title,description)
     }
 }
--- task-management/src/tasks/tasks.service.ts
+++ task-management/src/tasks/tasks.service.ts
@@ -1,10 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
-    private tasks =[]
-    getAllTask() {
+    private tasks:Task[] =[]
+
+    getAllTask() :Task[] {
         return this.tasks
     }
 
+    createTask(title:string,description:string):Task{
+
+        const id = new Date().toString();
+        const task:Task={
+            id,
+            title,
+            description,
+            status:TaskStatus.OPEN
+        }
+        
+        this.tasks.push(task)
+        return task
+    }
 }
