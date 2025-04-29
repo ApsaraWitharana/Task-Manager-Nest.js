9c75c652be911abd78f0aa4d33c066cb0c843854
ApsaraWitharana <asachini095@gmail.com>
2025-04-29 21:19:08 +0530

    add- delete task api coll

--- task-management/src/tasks/tasks.controller.ts
+++ task-management/src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post,Body, Param } from '@nestjs/common';
+import { Controller, Get, Post,Body, Param, Delete } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task } from './task.model';
 import { title } from 'process';
@@ -28,5 +28,12 @@ export class TasksController {
         @Body('description') description:string
     ):Task{
     return this.taskService.createTask(title,description)
+    }
+
+    //TODO:Delete Task
+    @Delete('/:id')
+    deleteTask(@Param('id') id:string){
+      this.taskService.deleteTask(id);
+      return `Task ${id} is delete Successfully!!`
     }
 }
